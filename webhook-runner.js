f6e6e686fc4f7f855b3c65b1789f3fc3eb017e53
tyage <namatyage@gmail.com>
2018-10-28 21:58:23 +0900

    remove config and receive all data from request

--- webhook-runner.js
+++ webhook-runner.js
@@ -1,19 +1,19 @@
 const http = require('http')
 const { calcScore } = require('./lib/calc-score.js')
-const config = require('./config.js')
 
 process.on('message', async (data) => {
-  const { id, team, problem } = data
-  const results = await calcScore(team, problem)
+  const { id, callback_url, callback_authorization_token } = data
+  const results = await calcScore(data)
 
   const postData = {
     id,
     results
   }
-  const req = http.request(config.web.webhookEndpoint, {
+  const req = http.request(callback_url, {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json'.
+      'Authorization': `Bearer ${callback_authorization_token}`
     }
   })
   req.write(JSON.stringify(postData))
