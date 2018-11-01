ea63eb26fb1abf01eea423a2638060c48b2462bc
icchy <icchyr@gmail.com>
2018-11-02 00:21:39 +0900

    validate paramater

--- webhook-server.js
+++ webhook-server.js
@@ -2,7 +2,29 @@ const http = require('http')
 const fs = require('fs')
 const child_process = require('child_process')
 
+const validData = (data) => {
+  params = [
+    'id',
+    'trials_count',
+    'timeout',
+    'docker_compose',
+    'registry_host',
+    'admin_username',
+    'admin_password',
+    'flag_template',
+    'callback_url',
+    'callback_authorization_token'
+  ]
+  const missingparam = params.find((el) => {
+    data[el] === undefined
+  })
+
+  return missingparam === undefined
+}
+
 const processData = (data) => {
+  if (!validData(data)) throw 'invalid data';
+
   const child = child_process.fork('./webhook-runner')
   child.send(data)
 }
