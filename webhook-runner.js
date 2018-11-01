7699e0a3e89ab10438ae2f77d35df33b74329e69
tyage <namatyage@gmail.com>
2018-11-02 03:36:52 +0900

    difference worker and child on webhook-runner

--- webhook-runner.js
+++ webhook-runner.js
@@ -1,34 +1,36 @@
 const http = require('http')
 const { URL } = require('url')
-const { calcScore } = require('./lib/calc-score.js')
+const { calcScore, calcScoreOnWorker } = require('./lib/calc-score.js')
 const cluster = require('cluster')
 
 console.log(cluster.isMaster)
 
-process.on('message', async (data) => {
-  if (cluster.isWorker) return;
-  
-  const { id, callback_url, callback_authorization_token } = data
-  const { succeeded, failed } = await calcScore(data)
+if (cluster.isMaster) { 
+  process.on('message', async (data) => {
+    const { id, callback_url, callback_authorization_token } = data
+    const { succeeded, failed } = await calcScore(data)
 
-  const postData = {
-    schedule_uuid: id,
-    succeeded,
-    failed
-  }
-
-  const url = new URL(callback_url)
-  const req = http.request({
-    hostname: url.hostname,
-    protocol: url.protocol,
-    path: url.pathname,
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${callback_authorization_token}`
+    const postData = {
+      schedule_uuid: id,
+      succeeded,
+      failed
     }
+
+    const url = new URL(callback_url)
+    const req = http.request({
+      hostname: url.hostname,
+      protocol: url.protocol,
+      path: url.pathname,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${callback_authorization_token}`
+      }
+    })
+    req.write(JSON.stringify(postData))
+    req.end()
+    console.debug('[postData]', JSON.stringify(postData))
   })
-  req.write(JSON.stringify(postData))
-  req.end()
-  console.debug('[postData]', JSON.stringify(postData))
-})  
+} else {
+  calcScoreOnWorker()
+}
