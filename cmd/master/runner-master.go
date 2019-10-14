4ed28093e7f33b7638d57b939650362ef36d1e82
icchy <icchyr@gmail.com>
2019-10-14 23:52:03 +0900

    init db

--- cmd/master/runner-master.go
+++ cmd/master/runner-master.go
@@ -5,10 +5,20 @@ import (
 	"log"
 
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"gitlab.com/CBCTF/bullseye-runner/pkg/master"
 	"gitlab.com/CBCTF/bullseye-runner/pkg/master/handler"
 )
+
+func initDB(db *gorm.DB) {
+	db.AutoMigrate(
+		&master.Schedule{},
+		&master.Round{},
+		&master.Result{},
+	)
+}
 
 func main() {
 	flag.Parse()
@@ -17,6 +27,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to open db: %v", err)
 	}
+	initDB(db)
 
 	e := echo.New()
 	e.Use(middleware.Logger())
@@ -25,6 +36,9 @@ func main() {
 	e.GET("/", handler.Index)
 	e.GET("/schedule", handler.GetSchedule(db))
 	e.POST("/schedule", handler.PostSchedule(db))
+
+	// notification endpoint for docker-registry
+	e.Any("/notification", handler.Notification(db))
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
